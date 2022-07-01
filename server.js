92af1c5739ea9b1cf9c1e9cc227ab1d4426f2138
Rodrigo Henrique <rodhenrc@outlook.com>
2022-07-01 13:19:11 -0300

    Alteração no arquivo server

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ const cors = require("cors");
 const app = express();
 const authRoutes = require("./app/routes/auth.routes");
 const userRoutes = require("./app/routes/user.routes");
-const port = 8080;
+const port = process.env.PORT || 8080;
 
-app.use(cors());
+app.use(cors({ credentials: true, origin: true }));
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
