8d9d30b82a0b2a74296aec6ebc62622373337010
Rodrigo Henrique <rodhenrc@outlook.com>
2022-07-01 17:28:02 -0300

    Mudança no arquivo de configuração do servidor

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -11,6 +11,30 @@ const connection = mysql.createConnection({
 connection.connect((error) => {
   if (error) {
     throw error;
+  } else {
+    connection.query(`use ${process.env.DB_DATABASE}`, (err, result) => {
+      if (err) {
+        throw err;
+      } else {
+        connection.query(
+          "CREATE TABLE IF NOT EXISTS Users(user_id INT AUTO_INCREMENT PRIMARY KEY, user_name VARCHAR(30) NOT NULL UNIQUE, user_password VARCHAR(255) NOT NULL, user_email VARCHAR(255) NOT NULL UNIQUE);",
+          (err, result) => {
+            if (err) {
+              throw err;
+            } else {
+              connection.query(
+                "CREATE TABLE IF NOT EXISTS Tasks(task_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, user_name VARCHAR(30) NOT NULL, task_uuid CHAR(36) NOT NULL UNIQUE, task_desc JSON NOT NULL, FOREIGN KEY(user_name) REFERENCES Users(user_name));",
+                (err, result) => {
+                  if (err) {
+                    throw err;
+                  }
+                }
+              );
+            }
+          }
+        );
+      }
+    });
   }
 });
 
